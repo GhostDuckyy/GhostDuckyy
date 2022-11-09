401bfe2bdc91f278b4ac7277629b1e7469249d36
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-11-09 23:25:21 +0800

    Create source.lua

---
+++ obfuscator/source.lua
@@ -0,0 +1,2 @@
+local func = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/obfuscator/main/kosuke14/RBXLuaObfuscator/obfuscator.lua"))()
+return func(source, customVar, watermark)
