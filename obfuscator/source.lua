4af6e04feb82939ac1f17e3fe4016f6d4ea4e768
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-11-09 23:32:39 +0800

    Update source.lua

--- obfuscator/source.lua
+++ obfuscator/source.lua
@@ -1,2 +1,4 @@
 local func = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/obfuscator/main/kosuke14/RBXLuaObfuscator/obfuscator.lua"))()
-return func(source, customVar, watermark)
+local obf = func(source, customVar, watermark)
+setclipboard(obf)
+return obf
