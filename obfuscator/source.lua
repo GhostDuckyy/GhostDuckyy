efda251e53365daa90c54388e3cfc733fd66771a
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-12-16 22:47:31 +0800

    Delete obfuscator directory

--- obfuscator/source.lua
+++
@@ -1,4 +0,0 @@
-local func = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/obfuscator/main/kosuke14/RBXLuaObfuscator/obfuscator.lua"))()
-local obf = func(source, customVar, watermark)
-setclipboard(obf)
-return obf
