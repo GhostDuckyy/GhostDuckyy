60e93062ef59e8acee2771a3c818b6f93ad6b4fc
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-02-09 13:42:52 +0800

    Create base64.lua

---
+++ Tool/base64.lua
@@ -0,0 +1,14 @@
+local base64 = {}
+local Encode = (syn and syn.crypt.base64.encode) or (crypt and (crypt.base64encode or crypt.base64.encode or crypt.base64_encode)) or base64.encode or base64_encode or function(...) error("Missing 'crypt' library") return end
+local Decode = (syn and syn.crypt.base64.decode) or (crypt and (crypt.base64decode or crypt.base64.decode or crypt.base64_decode)) or base64.decode or base64_decode or function(...) error("Missing 'crypt' library") return end
+
+
+function base64:encode(data, key)
+    Encode(data, key)
+end
+
+function base64:decode(data, key)
+    Decode(data, key)
+end
+
+return base64
