1d34b30fe87d3fe55fbf3220b465e9b0460d7d98
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-02-09 13:21:05 +0800

    Delete text.php

--- text.php
+++
@@ -1,7 +0,0 @@
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false) {
-  // code to be executed if the user is using Chrome
-  echo "You are using Chrome!";
-} else {
-  // code to be executed if the user is not using Chrome
-  echo "You are not using Chrome.";
-}
