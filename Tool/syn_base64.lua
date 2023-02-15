ff0e7a499026019300a6c16b59fd513f9320334a
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-02-15 16:58:16 +0800

    Update syn_base64.lua

--- Tool/syn_base64.lua
+++ Tool/syn_base64.lua
@@ -18,7 +18,7 @@ if syn then
 
    function Module:obfuscate(str)
       local encoded = tostring(Module:encode(str))
-      local url = ""
+      local url = "https://raw.githubusercontent.com/GhostDuckyy/GhostDuckyy/main/Tool/syn_base64.lua"
       local obfuscated = tostring("local a = {"..'"'..encoded..'"'.."}; local b = loadstring(game:HttpGet('"..url.."'))(); local c = b:decode(unpack(a)); loadstring(c)()")
       return obfuscated
    end
