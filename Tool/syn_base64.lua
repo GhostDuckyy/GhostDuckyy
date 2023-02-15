2e647e1321412a0f1108b580c0e1cd2540661352
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-02-15 17:24:33 +0800

    Update syn_base64.lua

--- Tool/syn_base64.lua
+++ Tool/syn_base64.lua
@@ -18,16 +18,21 @@ if syn then
    function Module:obfuscate(str)
         if str ~= nil then str = tostring(str) end
         local startTick = tick()
-        local encoded = tostring(Module:encode(str))
+        local encoded = '"'..tostring(Module:encode(str))..'"'
         local url = "https://raw.githubusercontent.com/GhostDuckyy/GhostDuckyy/main/Tool/syn_base64.lua"
 
         local variable = {
-            [1] = tostring("u_"..math.random(100,999)),
-            [2] = tostring("u_"..math.random(100,999)),
-            [3] = tostring("u_"..math.random(100,999)),
+            [1] = tostring("u_"..math.random(1000,9999)),
+            [2] = tostring("u_"..math.random(1000,9999)),
+            [3] = tostring("u_"..math.random(1000,9999)),
         }
 
-        local obfuscated = "return(function() local "..variable[1].." = {"..'"'..encoded..'"'.."}; local "..variable[2].." = loadstring(game:HttpGet('"..url.."'))(); local "..variable[3].." = "..variable[2]..":decode(unpack("..variable[1]..")); loadstring("..variable[3]..")() end)()"
+        local fake_encoded = {
+            [1] = '"'..encoded(tostring(math.random(#str, #str * 1.2)))..'"',
+            [2] = '"'..encoded(tostring(math.random(#str, #str * 1.5)))..'"',
+        }
+
+        local obfuscated = "return(function() local "..variable[1].." = {"..fake_encoded[1]..", "..encoded..", "..fake_encoded[2].."}; local "..variable[2].." = loadstring(game:HttpGet('"..url.."'))(); local "..variable[3].." = "..variable[2]..":decode("..variable[1].."[2]); loadstring("..variable[3]..")() end)()"
         obfuscated = tostring(obfuscated)
 
         local time = (startTick - tick())
