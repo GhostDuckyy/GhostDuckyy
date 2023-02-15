41c1d43eba0f14760565037143e38ca9acb1cf38
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-02-15 17:11:29 +0800

    Update syn_base64.lua

--- Tool/syn_base64.lua
+++ Tool/syn_base64.lua
@@ -1,32 +1,33 @@
 if syn then
    local Module = {}
-   Module = {}
 
    function Module:encode(v)
-      local encode = syn.crypt.base64.encode
-      if encode then
-         return encode(tostring(v))
-      end
+        local encode = syn.crypt.base64.encode
+        if encode then
+            return encode(tostring(v))
+        end
    end
 
    function Module:decode(v)
-      local decode = syn.crypt.base64.decode
-      if decode then
-         return decode(tostring(v))
-      end
+        local decode = syn.crypt.base64.decode
+        if decode then
+            return decode(tostring(v))
+        end
    end
 
    function Module:obfuscate(str)
-      local startTick = tick()
-      local encoded = tostring(Module:encode(str))
-      local url = "https://raw.githubusercontent.com/GhostDuckyy/GhostDuckyy/main/Tool/syn_base64.lua"
-      local obfuscated = tostring("local a = {"..'"'..encoded..'"'.."}; local b = loadstring(game:HttpGet('"..url.."'))(); local c = b:decode(unpack(a)); loadstring(c)()")
-      
-      local time = (startTick - tick())
-      warn("Done obfuscation in "..time.."ticks")
-      
-      if setclipboard then setclipboard(obfuscated) end
-      return obfuscated
+        if str ~= nil then str = tostring(str) end
+        local startTick = tick()
+        local encoded = tostring(Module:encode(str))
+        local url = "https://raw.githubusercontent.com/GhostDuckyy/GhostDuckyy/main/Tool/syn_base64.lua"
+        local obfuscated = "return(function() local a = {"..'"'..encoded..'"'.."}; local b = loadstring(game:HttpGet('"..url.."'))(); local c = b:decode(unpack(a)); loadstring(c)() end)()"
+        obfuscated = tostring(obfuscated)
+
+        local time = (startTick - tick())
+        warn("Done obfuscation in "..time.."ticks")
+
+        if setclipboard then setclipboard(obfuscated) end
+        return obfuscated
    end
 
    return Module
