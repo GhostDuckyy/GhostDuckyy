1f755a79191bee9b0be223f18cb799242027e624
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-02-28 22:05:43 +0800

    Create FPS_booster.lua

---
+++ Tool/FPS_booster.lua
@@ -0,0 +1,107 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+local startTick = tick()
+local IgnoreCharacters
+local CustomList
+
+if typeof(getgenv().Connections):lower() == "table" then
+    for i,v in next, getgenv().Connections do
+        task.spawn(function()
+            pcall(function()
+                v:Disconnect()
+            end)
+        end)
+    end
+    getgenv().Connections = {}
+else
+    getgenv().Connections = {}
+end
+
+local PartsList = {
+    "Part",
+    "BasePart",
+    "MeshPart",
+    ["BlackList"] = {
+        "HumanoidRootPart",
+        "Head",
+        "Torso",
+        "Left Arm",
+        "Right Arm",
+        "Left Leg",
+        "Right Leg",
+        "UpperTorso",
+        "LowerTorso"
+    }
+}
+
+local DestroyList = {
+    "SurfaceGui",
+    "BillboardGui",
+    "Decal",
+    "Texture",
+    "Accessory",
+}
+
+local function checkInstance(v)
+    task.spawn(function()
+        task.wait(.1)
+        IgnoreCharacters = (typeof(getgenv().IgnoreCharacters):lower() == "boolean" and getgenv().IgnoreCharacters) or false
+        CustomList = (typeof(getgenv().CustomList):lower() == "table" and getgenv().CustomList) or {}
+        if typeof(v) ~= "Instance" or (v == nil or v.Parent == nil) then return end
+        if IgnoreCharacters and (v.Parent:IsA("Model") and game:GetService("Players"):GetPlayerFromCharacter(v.Parent) ~= nil) then return end
+
+        if table.find(PartsList, v.ClassName) then
+            pcall(function()
+                if not table.find(PartsList.BlackList, v.Name) then
+                    v.Material = Enum.Material.SmoothPlastic
+                end
+            end)
+        elseif table.find(DestroyList, v.ClassName) then
+            pcall(function()
+                v:Destroy()
+            end)
+        else
+            for i2 = 1, #CustomList do
+                local currentArray = CustomList[i2]
+                if type(currentArray) == "table" then
+                    local Name = currentArray.Name or currentArray.name or nil
+                    local ClassName = currentArray.ClassName or currentArray.classname or nil
+
+                    if type(Name) == "string" then
+                        pcall(function()
+                            if v == nil or v.Parent == nil then return end
+                            local lower = v.Name:lower()
+                            if lower == Name:lower() or (string.find(lower, Name:lower()) or string.match(lower, Name:lower())) then
+                                if not table.find(PartsList.BlackList, v.Name) then
+                                    v:Destroy()
+                                end
+                            end
+                        end)
+                    end
+
+                    if type(ClassName) == "string" then
+                        pcall(function()
+                            if v == nil or v.Parent == nil then return end
+                            if ClassName:lower() == "model" and (v:IsA("Model") and game:GetService("Players"):GetPlayerFromCharacter(v) ~= nil) then return end
+                            local lower = v.ClassName:lower()
+                            if lower == ClassName:lower() or (string.find(lower, ClassName:lower()) or string.match(lower, ClassName:lower())) then
+                                if not table.find(PartsList.BlackList, v.Name) then
+                                    v:Destroy()
+                                end
+                            end
+                        end)
+                    end
+                end
+            end
+        end
+    end)
+end
+
+for i,v in ipairs(workspace:GetDescendants()) do
+    checkInstance(v)
+end
+
+local DescendantAdded = workspace.DescendantAdded:Connect(checkInstance)
+table.insert(getgenv().Connections, DescendantAdded)
+local Time = tostring(startTick - tick())
+warn("FPS Boost Loaded in "..Time.." tick(s)")
