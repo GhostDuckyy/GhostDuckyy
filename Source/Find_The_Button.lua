515b24407b67af81855358db6240d6e0ad490f46
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-03-03 01:29:23 +0800

    Update Find_The_Button.lua

--- Source/Find_The_Button.lua
+++ Source/Find_The_Button.lua
@@ -2,18 +2,18 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-if workspace:FindFirstChild("CharmsFolder") then
-    workspace:FindFirstChild("CharmsFolder"):Destroy()
+if game:GetService("CoreGui"):FindFirstChild("CharmsFolder") then
+    game:GetService("CoreGui"):FindFirstChild("CharmsFolder"):Destroy()
 end
 
-local Folder = Instance.new("Folder", workspace)
+local Folder = Instance.new("Folder", game:GetService("CoreGui"))
 Folder.Name = "CharmsFolder"
 
 function Charms(v)
     if Folder == nil then return end
     if typeof(v) ~= "Instance" then return end
     local HighLight = Instance.new("Highlight")
-    HighLight.Parent = workspace:WaitForChild("CharmsFolder")
+    HighLight.Parent = Folder
     HighLight.Name = game:GetService("HttpService"):GenerateGUID(true)
     HighLight.Adornee = v
 
@@ -21,7 +21,7 @@ function Charms(v)
     HighLight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
 
     HighLight.FillColor = Color3.new(1, 1, 1)
-    HighLight.OutlineColor = Color3.new(1, 0, 0)
+    HighLight.OutlineColor = Color3.new(0, 1, 0)
 
     HighLight.FillTransparency = .3
     HighLight.OutlineTransparency = .1
