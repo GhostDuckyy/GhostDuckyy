7f00de3857ee9af2f11f7e9b8937fdc0b2f8c2eb
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-03-04 01:03:38 +0800

    Update FPS_booster.lua

--- Tool/FPS_booster.lua
+++ Tool/FPS_booster.lua
@@ -8,7 +8,7 @@ local Character = (LocalPlayer.Character or LocalPlayer.CharacterAppearanceLoade
 
 --// Env
 local startTick = tick()
-local IgnoreCharacters, CustomList
+local IgnoreCharacters, CustomList, IgnoreList
 
 --// Remove connections
 if typeof(getgenv().Connections):lower() == "table" then
@@ -23,6 +23,12 @@ else
 end
 
 --// List
+local default_Setting = {
+    IgnoreCharacters = {Self = true, Others = false, RemoveClothes = false},
+    CustomList = {},
+    IgnoreList = {{ClassName = "Folder"}, {ClassName = "Model"}},
+}
+
 local PartsList = {
     "Part",
     "BasePart",
@@ -57,30 +63,88 @@ local DestroyList = {
     "Explosion",
 }
 
-local IgnoreClassName = {
-    "Folder",
-    "Model",
-}
-
 --// function
+local function checkParent(value, Parent)
+    if typeof(value) ~= "Instance" then return end
+
+    if typeof(Parent) == "Instance" and Parent ~= nil then
+        for i,v in ipairs(Parent:GetDescendants()) do
+            if v == value then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+
+local function checkIgnoreList(value)
+    if typeof(value) ~= "Instance" then return end
+
+    for i, v in next, (IgnoreList) do
+        if type(v) == "table" then
+            local Namelower = value.Name:lower()
+            local Classlower = value.ClassName:lower()
+
+            local Name = v.Name or v.name or nil
+            local ClassName = v.ClassName or v.classname or nil
+
+            if (type(Name) == "string" and type(ClassName) == "string") then
+                if (Namelower == Name:lower() or string.find(Namelower, Name:lower()) or string.match(Namelower, Name:lower())) and (value:IsA(ClassName) or Classlower == ClassName:lower() or string.find(Classlower, ClassName:lower()) or string.match(Classlower, ClassName:lower())) then
+                    return true
+                end
+            elseif type(Name) == "string" then
+                if (Namelower == Name:lower() or string.find(Namelower, Name:lower()) or string.match(Namelower, Name:lower())) then
+                    return true
+                end
+            elseif type(ClassName) == "string" then
+                if value:IsA(ClassName) or (Classlower == ClassName:lower() or string.find(Classlower, ClassName:lower()) or string.match(Classlower, ClassName:lower())) then
+                    return true
+                end
+            end
+        end
+    end
+
+    return false
+end
+
 local function checkInstance(v)
     pcall(task.spawn(function()
         task.wait(.1)
-        IgnoreCharacters = (typeof(getgenv().IgnoreCharacters):lower() == "table" and getgenv().IgnoreCharacters) or {Self = true, Others = false, Clothes = true}
-        CustomList = (typeof(getgenv().CustomList):lower() == "table" and getgenv().CustomList) or {}
+        if type(getgenv().Setting):lower() == "table" then
+            IgnoreCharacters = (typeof(getgenv().Setting.IgnoreCharacters):lower() == "table" and getgenv().Setting.IgnoreCharacters) or default_Setting.IgnoreCharacters
+            CustomList = (typeof(getgenv().Setting.CustomList):lower() == "table" and getgenv().Setting.CustomList) or default_Setting.CustomList
+            IgnoreList = (typeof(getgenv().Setting.IgnoreList):lower() == "table" and getgenv().Setting.IgnoreList) or default_Setting.IgnoreList
+        else
+            IgnoreCharacters = default_Setting.IgnoreCharacters
+            CustomList = default_Setting.CustomList
+            IgnoreList = default_Setting.IgnoreList
+        end
 
         if typeof(v) ~= "Instance" or (v == nil or v.Parent == nil) then return end
-        if (v.Parent ~= nil and v.Parent == workspace.CurrentCamera) then return end
-        if table.find(IgnoreClassName, v.ClassName) then return end
+        if checkParent(v, workspace.CurrentCamera) then return end
+        if checkIgnoreList(v) then return end
 
-        if IgnoreCharacters.Self then
-            local GetPlayerFromCharacter = Players:GetPlayerFromCharacter(v.Parent)
-            if (v.Parent ~= nil and (GetPlayerFromCharacter ~= nil and GetPlayerFromCharacter == LocalPlayer)) then return end
+        if IgnoreCharacters.Self and Character ~= nil then
+            if checkParent(v, Character) then
+                return
+            end
         end
 
         if IgnoreCharacters.Others then
-            local GetPlayerFromCharacter = Players:GetPlayerFromCharacter(v.Parent)
-            if (v.Parent ~= nil and (GetPlayerFromCharacter ~= nil and GetPlayerFromCharacter ~= LocalPlayer)) then return end
+            local GrabPlayers = Players:GetPlayers()
+
+            for i = 1, #GrabPlayers do
+                local currentArray = GrabPlayers[i]
+
+                if currentArray ~= nil then
+                    if currentArray ~= LocalPlayer and currentArray.Character ~= nil then
+                        if checkParent(v, currentArray.Character) then
+                            return
+                        end
+                    end
+                end
+            end
         end
 
         if table.find(PartsList, v.ClassName) then
@@ -88,10 +152,20 @@ local function checkInstance(v)
                 v.Material = Enum.Material.SmoothPlastic
             end
         elseif table.find(DestroyList, v.ClassName) then
-            if v:IsA("Decal") and (v.Parent ~= nil and table.find(PartsList.BlackList, v.Parent.Name)) then
-                return
+            if v:IsA("Decal") then
+                if (v.Parent ~= nil and table.find(PartsList.BlackList, v.Parent.Name)) then
+                    return
+                else
+                    v.Transparency = 1
+                end
+            elseif v:IsA("BillboardGui") then
+                if (v.Parent ~= nil and table.find(PartsList.BlackList, v.Parent.Name)) then
+                    return
+                else
+                    v.Enabled = false
+                end
             elseif v:IsA("Shirt") or v:IsA("Pants") then
-                if not IgnoreCharacters.Clothes then v:Destroy() else return end
+                if IgnoreCharacters.RemoveClothes then v:Destroy() else return end
             end
 
             v:Destroy()
@@ -102,7 +176,14 @@ local function checkInstance(v)
                     local Name = currentArray.Name or currentArray.name or nil
                     local ClassName = currentArray.ClassName or currentArray.classname or nil
 
-                    if type(Name) == "string" then
+                    if (type(Name) == "string" and type(ClassName) == "string") then
+                        local Namelower = v.Name:lower()
+                        local Classlower = v.ClassName:lower()
+
+                        if (Namelower == Name:lower() or string.find(Namelower, Name:lower()) or string.match(Namelower, Name:lower())) and (v:IsA(ClassName) or Classlower == ClassName:lower() or string.find(Classlower, ClassName:lower()) or string.match(Classlower, ClassName:lower())) then
+                            return true
+                        end
+                    elseif type(Name) == "string" then
                         if v == nil or v.Parent == nil then return end
                         local lower = v.Name:lower()
                         if lower == Name:lower() or (string.find(lower, Name:lower()) or string.match(lower, Name:lower())) then
@@ -110,13 +191,11 @@ local function checkInstance(v)
                                 v:Destroy()
                             end
                         end
-                    end
-
-                    if type(ClassName) == "string" then
+                    elseif type(ClassName) == "string" then
                         if v == nil or v.Parent == nil then return end
-                        if ClassName:lower() == "model" and (v:IsA("Model") and Players:GetPlayerFromCharacter(v) ~= nil) then return end
+
                         local lower = v.ClassName:lower()
-                        if lower == ClassName:lower() or (string.find(lower, ClassName:lower()) or string.match(lower, ClassName:lower())) then
+                        if v:IsA(ClassName) or (lower == ClassName:lower() or string.find(lower, ClassName:lower()) or string.match(lower, ClassName:lower())) then
                             if not table.find(PartsList.BlackList, v.Name) then
                                 v:Destroy()
                             end
@@ -129,12 +208,12 @@ local function checkInstance(v)
 end
 
 --// Source
+local DescendantAdded = workspace.DescendantAdded:Connect(checkInstance)
+table.insert(getgenv().Connections, DescendantAdded)
+
 for i,v in ipairs(workspace:GetDescendants()) do
     checkInstance(v)
 end
-
-local DescendantAdded = workspace.DescendantAdded:Connect(checkInstance)
-table.insert(getgenv().Connections, DescendantAdded)
 
 --// Loaded time
 local Time = tostring(startTick - tick())
