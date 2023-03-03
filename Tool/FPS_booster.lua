1bd713b1506548f640d929428e8d1a75eb50f036
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-03-04 01:05:34 +0800

    Update FPS_booster.lua

--- Tool/FPS_booster.lua
+++ Tool/FPS_booster.lua
@@ -68,8 +68,10 @@ local function checkParent(value, Parent)
     if typeof(value) ~= "Instance" then return end
 
     if typeof(Parent) == "Instance" and Parent ~= nil then
-        for i,v in ipairs(Parent:GetDescendants()) do
-            if v == value then
+        local Descendants = Parent:GetDescendants()
+        for i = 1, #Descendants do
+            local currentArray = Descendants[i]
+            if currentArray ~= nil and currentArray == value then
                 return true
             end
         end
