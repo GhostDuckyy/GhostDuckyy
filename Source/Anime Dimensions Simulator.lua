57e2dab4cb87c13d5d3e511d32696c326a2dca17
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-04-14 17:29:41 +0800

    Update Anime Dimensions Simulator.lua

--- Source/Anime Dimensions Simulator.lua
+++ Source/Anime Dimensions Simulator.lua
@@ -17,82 +17,80 @@ local MainRemoteEvent         =  ReplicatedStorage:WaitForChild("RemoteEvents"):
 local MainRemoteFunction      =  ReplicatedStorage:WaitForChild("RemoteFunctions"):WaitForChild("MainRemoteFunction")
 
 --// Settings \\--
+getgenv().CurrentTween = nil
 getgenv().Settings = (type(getgenv().Settings) == "table" and getgenv().Settings) or {
-   AutoFarm = true,
-   AutoRetry = true,
-   DebugMode = false,
+    AutoFarm        =   true,
+    AutoRetry       =   true,
+    DebugMode       =   false,
 }
 
-if not Settings.AutoFarm then
+if (not Settings.AutoFarm) then
    Settings.AutoFarm = true
 end
 
-getgenv().CurrentTween = nil
-
 --// Tween \\--
 local function CancelTween()
-   if (not CurrentTween) then
-      CurrentTween:Pause()
-      CurrentTween:Cancel()
-      CurrentTween = nil
-   end
+    if (not CurrentTween) then
+        CurrentTween:Pause()
+        CurrentTween:Cancel()
+        CurrentTween = nil
+    end
 end
 
 local function Tween(Time: number, prop: table)
-   if type(Time) ~= "number" then Time = 1 end
-   if type(prop) ~= "table" then return end
+    if type(Time) ~= "number" then Time = 1 end
+    if type(prop) ~= "table" then return end
 
-   if (CurrentTween) then
-      CancelTween()
-   end
+    if (CurrentTween) then
+        CancelTween()
+    end
 
-   local Root = GetRoot()
-   if (not Root) then
-      while task.wait() do
-         Root = GetRoot()
-         if (Root) then break end
-      end
-   end
-   Root.Anchored = false
+    local Root = GetRoot()
+    if (not Root) then
+        while task.wait() do
+            Root = GetRoot()
+            if (Root) then break end
+        end
+    end
+    Root.Anchored = false
 
-   local distance = (Root.CFrame.Position - prop.CFrame.Position).Magnitude
+    local distance = (Root.CFrame.Position - prop.CFrame.Position).Magnitude
 
-   if distance <= 15 then
-      Root.CFrame = prop.CFrame
-      Root.Anchored = true
-      task.wait(.1)
-   else
-      CurrentTween = TweenService:Create(Root, TweenInfo.new(Time, Enum.EasingStyle.Linear, Enum.EasingStyle.Linear), prop)
-      CurrentTween.Completed:Connect(function()
-         CurrentTween = nil
-         if (Root) then
-            Root.Anchored = true
-         end
-      end)
+    if (distance <= 15) then
+        Root.CFrame = prop.CFrame
+        Root.Anchored = true
+        task.wait(.1)
+    else
+        CurrentTween = TweenService:Create(Root, TweenInfo.new(Time, Enum.EasingStyle.Linear, Enum.EasingStyle.Linear), prop)
+        CurrentTween.Completed:Connect(function()
+            CurrentTween = nil
+            if (Root) then
+                Root.Anchored = true
+            end
+        end)
 
-      CurrentTween:Play()
-      CurrentTween.Completed:Wait()
-   end
+        CurrentTween:Play()
+        CurrentTween.Completed:Wait()
+    end
 end
 
 --// Functions \\--
 function GetCharacter()
-   local RespawnTimerFrame = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("UniversalGui"):WaitForChild("UniversalCenterUIFrame"):WaitForChild("RespawnTimerFrame")
-   if (RespawnTimerFrame and RespawnTimerFrame.Visible) then
-      return nil
-   end
-   return (LocalPlayer.Character ~= nil and LocalPlayer.Character) or LocalPlayer.CharacterAdded:Wait()
+    local RespawnTimerFrame = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("UniversalGui"):WaitForChild("UniversalCenterUIFrame"):WaitForChild("RespawnTimerFrame")
+    if (RespawnTimerFrame and RespawnTimerFrame.Visible) then
+        return nil
+    end
+    return (LocalPlayer.Character ~= nil and LocalPlayer.Character) or LocalPlayer.CharacterAdded:Wait()
 end
 
 function GetRoot()
-   local Character = GetCharacter()
-   if (Character) then
-      task.wait(.1)
-      Character:WaitForChild("HumanoidRootPart", 9e9)
-      local Root = Character:FindFirstChild("HumanoidRootPart")
-      return Root
-   end
-   return nil
+    local Character = GetCharacter()
+    if (Character) then
+        Character:WaitForChild("HumanoidRootPart", 9e9)
+        local Root = Character:FindFirstChild("HumanoidRootPart")
+        return Root
+    end
+    return nil
 end
 
 function GetClosestEnemy()
@@ -101,29 +99,29 @@ function GetClosestEnemy()
    local Root = GetRoot()
    local Childrens = Monsters:GetChildren()
 
-   if not Root then return nil end
-   if #Childrens <= 0 then return nil end
+   if (not Root) then return nil end
+   if (#Childrens <= 0) then return nil end
 
    for i = 1, #Childrens do
-      local Child = Childrens[i]
+        local Child = Childrens[i]
 
-      if (Child) then
-         if not Child:FindFirstChildOfClass("Humanoid") then continue end
-         if not Child:FindFirstChild("HumanoidRootPart") then continue end
-         if Child:FindFirstChildOfClass("Humanoid").Health <= 0 then continue end
+        if (Child) then
+            if not Child:FindFirstChildOfClass("Humanoid") then continue end
+            if not Child:FindFirstChild("HumanoidRootPart") then continue end
+            if Child:FindFirstChildOfClass("Humanoid").Health <= 0 then continue end
 
-         if Child:FindFirstChildOfClass("Highlight") then
-            Enemy = Child
-            return Enemy
-         end
+            if Child:FindFirstChildOfClass("Highlight") then
+                Enemy = Child
+                return Enemy
+            end
 
-         local distance = (Root.Position - Child.HumanoidRootPart.Position).Magnitude
+            local distance = (Root.Position - Child.HumanoidRootPart.Position).Magnitude
 
-         if distance < Last_distance then
-            Enemy = Child
-            Last_distance = distance
-         end
-      end
+            if distance < Last_distance then
+                Enemy = Child
+                Last_distance = distance
+            end
+        end
    end
 
    return Enemy
@@ -147,38 +145,38 @@ function checkCD(number: number, assist: boolean)
 
    local SlotsHolder = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("UniversalGui"):WaitForChild("UniversalCenterUIFrame"):WaitForChild("SlotsHolder")
 
+   if (not SlotsHolder) then debug_SendOutput("SlotsHolder is 'nil' \n") return end
    if assist then
-      if number >= 3 then return end
-      local Slot = SlotsHolder:WaitForChild("SkillAssist"..tostring(number))
-      if Slot and Slot.Visible then
-         local SkillName = Slot:WaitForChild("SkillName").Text
+        if number >= 3 then return end
+        local Slot = SlotsHolder:WaitForChild("SkillAssist"..tostring(number))
+        if Slot and Slot.Visible then
+            local SkillName = Slot:WaitForChild("SkillName").Text
 
-         if not SkillName.Visible then
-            return true
-         end
-      end
+            if not SkillName.Visible then
+                return true
+            end
+        end
    else
-      if number == 5 then
-         local Slot = SlotsHolder:WaitForChild("Skill"..tostring(number))
+        if number >= 6 then return end
+        local Slot = SlotsHolder:WaitForChild("Skill"..tostring(number))
 
-         if Slot and Slot.Visible then
-            local SkillName = Slot:WaitForChild("SkillName").Text
+        if (number == 5) then
+            if (Slot and Slot.Visible) then
+                local SkillName = Slot:WaitForChild("SkillName").Text
 
-            if SkillName:lower() ~= "skill 2" and not tonumber(SkillName) then
-               return true
+                if SkillName:lower() ~= "skill 2" and not tonumber(SkillName) then
+                return true
+                end
             end
-         end
-      else
-         local Slot = SlotsHolder:WaitForChild("Skill"..tostring(number))
+        elseif (4 >= number) then
+            if (Slot and Slot.Visible) then
+                local SkillName = Slot:WaitForChild("SkillName").Text
 
-         if Slot and Slot.Visible then
-            local SkillName = Slot:WaitForChild("SkillName").Text
-
-            if SkillName:lower() ~= "skill "..tostring(number) and not tonumber(SkillName) then
-               return true
+                if SkillName:lower() ~= "skill "..tostring(number) and not tonumber(SkillName) then
+                return true
+                end
             end
-         end
-      end
+        end
    end
 
    return false
@@ -186,7 +184,7 @@ end
 
 function useAbility(mode)
    local Root = GetRoot()
-   if not Root then return end
+   if (not Root) then return end
 
    task.spawn(function()
       if type(mode) == "string" and mode:lower() == "click" then
@@ -198,8 +196,11 @@ function useAbility(mode)
 end
 
 function IsEnded()
-   local CenterUIFrame =LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("UniversalGui"):WaitForChild("UniversalCenterUIFrame")
-   local ResultUIs = {[1] = CenterUIFrame:WaitForChild("ResultUI"), [2] = CenterUIFrame:WaitForChild("RaidResultUI")}
+   local CenterUIFrame = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("UniversalGui"):WaitForChild("UniversalCenterUIFrame")
+   local ResultUIs = {
+        [1] = CenterUIFrame:WaitForChild("ResultUI"),
+        [2] = CenterUIFrame:WaitForChild("RaidResultUI"),
+    }
 
    for i,v in pairs(ResultUIs) do
       if (v.Visible) then
@@ -222,34 +223,17 @@ function Leave()
    end)
 end
 
-function debugPrint(...)
-   if Settings.DebugMode then
-      local MSGs = {...}
-
-      for i,v in pairs(MSGs) do
-         print(tostring(v))
-      end
-   end
+function debug_SendOutput(...)
+    if (not Settings.DebugMode) then return end
+    local Outputs = {...}
+    task.spawn(function()
+        for _, v in next, (Outputs) do
+            print(tostring(v))
+         end
+    end)
 end
 
 --// Source \\--
-if (GetCharacter()) then
-    if (GetCharacter():WaitForChild("Head", 9e9)) then
-        local NameLabel = GetCharacter().Head:WaitForChild("PlayerHealthBarGui"):WaitForChild("PlayerName")
-        NameLabel.Text = "Made by Ghost-Ducky#7698"
-    end
-end
-
-LocalPlayer.CharacterAdded:Connect(function(newCharacter)
-    task.wait(.1)
-    if (newCharacter) then
-        if (newCharacter:WaitForChild("Head", 9e9)) then
-            local NameLabel = newCharacter.Head:WaitForChild("PlayerHealthBarGui"):WaitForChild("PlayerName")
-            NameLabel.Text = "Made by Ghost-Ducky#7698"
-        end
-    end
-end)
-
 task.spawn(function()
     local Enemy = GetClosestEnemy()
     while task.wait(.1) do
@@ -263,74 +247,86 @@ task.spawn(function()
         local Character, Root = GetCharacter(), GetRoot()
 
         if IsEnded() then
-            debugPrint("Game Ended")
+            debug_SendOutput("Game Ended")
 
             if Settings.AutoRetry then
-                debugPrint("Retry dungeon \n")
+                debug_SendOutput("Retry dungeon \n")
                 Retry()
                 continue
             else
-                debugPrint("Leave dungeon \n")
+                debug_SendOutput("Leave dungeon \n")
                 Leave()
                 continue
             end
         end
 
         if (Character and Root) and not IsEnded() then
+            pcall(task.spawn(function()
+                if (Character and Character:WaitForChild("Head", 90)) then
+                    local NameLabel = newCharacter.Head:WaitForChild("PlayerHealthBarGui"):WaitForChild("PlayerName")
+                    NameLabel.Text = "Made by Ghost-Ducky#7698"
+                end
+            end))
+
             if (Enemy and Enemy.Parent) then
-                local EnemyRoot = Enemy:WaitForChild("HumanoidRootPart")
+                local EnemyRoot = Enemy:FindFirstChild("HumanoidRootPart")
                 local EnemyHumanoid = Enemy:FindFirstChildOfClass("Humanoid")
+
+                if (not EnemyRoot or not EnemyHumanoid) then
+                    Enemy = nil
+                    continue
+                end
 
                 if (EnemyHumanoid and EnemyHumanoid.Health <= 0) then
                     Enemy = nil
                     continue
                 end
 
-                debugPrint("MoveTo: "..Enemy.Name, "\n")
+                debug_SendOutput("MoveTo: "..Enemy.Name.."\n")
                 Tween(1, {CFrame = CFrame.lookAt(EnemyRoot.CFrame.Position + Vector3.new(0, 5, 0), EnemyRoot.CFrame.Position) })
 
                 if checkCD(1, true) then
-                    debugPrint("UseAssist: 1 \n")
+                    debug_SendOutput("Use Assist: 1")
                     useAssist(1)
                 end
 
                 if checkCD(2, true) then
-                    debugPrint("UseAssist: 2 \n")
+                    debug_SendOutput("Use Assist: 2")
                     useAssist(2)
                 end
 
                 if (not checkCD(5) and not checkCD(4) and not checkCD(3) and not checkCD(2) and not checkCD(1)) then
-                    debugPrint("UseSkill: BasicAttack")
+                    debug_SendOutput("Use Skill: BasicAttack")
                     useAbility("click")
                     continue
                 end
 
                 if checkCD(5) then
-                    debugPrint("UseSkill: 5")
+                    debug_SendOutput("Use Skill: 5")
                     useAbility(5)
                     continue
                 end
 
                 if checkCD(4) then
-                    debugPrint("UseSkill: 4")
+                    debug_SendOutput("Use Skill: 4")
                     useAbility(4)
                     continue
                 end
 
                 if checkCD(3) then
-                    debugPrint("UseSkill: 3")
+                    debug_SendOutput("Use Skill: 3")
                     useAbility(3)
                     continue
                 end
 
                 if checkCD(2) then
-                    debugPrint("UseSkill: 2")
+                    debug_SendOutput("Use Skill: 2")
                     useAbility(2)
                     continue
                 end
 
                 if checkCD(1) then
-                    debugPrint("UseSkill: 1")
+                    debug_SendOutput("UseS kill: 1")
                     useAbility(1)
                     continue
                 end
